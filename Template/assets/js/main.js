9d7e9ff8f9c3231f387e7c2434bb8deb00cb5ebb
HarshalJadhav99 <harshalj.mobio@gmail.com>
2021-12-17 18:12:19 +0530

    master: added thecultivist project files here

---
+++ Template/assets/js/main.js
@@ -0,0 +1,65 @@
+// FIRST COLAPSE
+
+$(document).ready(function () {
+  $("#watch-me").click(function () {
+    $("#show-me:hidden").show("slow");
+  });
+  $("#watch-me").click(function () {
+    if ($("watch-me").prop("checked") === false) {
+      $("#show-me").hide();
+    }
+  });
+});
+// SECOND COLLAPSE
+$(document).ready(function () {
+  $("#watch-me2").click(function () {
+    $("#show-me2:hidden").show("slow");
+  });
+  $("#watch-me2").click(function () {
+    if ($("watch-me2").prop("checked") === false) {
+      $("#show-me2").hide();
+    }
+  });
+});
+//THIRD COLLAPSE
+$(document).ready(function () {
+  $("#watch-me3").click(function () {
+    $("#show-me3:hidden").show("slow");
+  });
+  $("#watch-me3").click(function () {
+    if ($("watch-me3").prop("checked") === false) {
+      $("#show-me3").hide();
+    }
+  });
+});
+//FOURTH COLLAPSE
+$(document).ready(function () {
+  $("#watch-me4").click(function () {
+    $("#show-me4:hidden").show("slow");
+  });
+  $("#watch-me4").click(function () {
+    if ($("watch-me4").prop("checked") === false) {
+      $("#show-me4").hide();
+    }
+  });
+});
+
+// $("#submit").click(function () {
+//   var name = $("#custom").val();
+//   var second = $("#gridRadios22").val();
+//   if (name == "" || second == "") {
+//     swal({
+//       title: "Good job!",
+//       text: "You clicked the button!",
+//       icon: "success",
+//       button: "ok",
+//     });
+//   } else {
+//     swal({
+//       title: "Good job! Form is submitted successfully",
+//       text: "You clicked the button!",
+//       icon: "success",
+//       button: "ok",
+//     });
+//   }
+// });
